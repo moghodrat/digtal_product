cef9e388680e71b4c7fffa2e96a359f3caef7bcc
ghodrat <meghodrat@gmail.com>
2022-11-04 20:46:22 +0330

    commit add user

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
     title = models.CharField(verbose_name=_('title'), max_length=50)
     description = models.TextField(verbose_name=_('description'), blank=True)
-    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categorise/')
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='categories/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
@@ -14,7 +14,10 @@ class Category(models.Model):
     class Meta:
         db_table = 'category'
         verbose_name = _('category')
-        verbose_name_plural = _('categorise')
+        verbose_name_plural = _('categories')
+
+    def __str__(self):
+        return self.title
 
 
 
@@ -32,9 +35,21 @@ class Product(models.Model):
         verbose_name = _('product')
         verbose_name_plural = _('products')
 
+    def __str__(self):
+        return self.title
+
 class File(models.Model):
-    product = models.ForeignKey('product', verbose_name=_('product'), on_delete=models.CASCADE)
+    FILE_AUDIO = 1
+    FILE_VIDEO = 2
+    FILE_PDF = 3
+    FILE_TYPES = (
+        (FILE_AUDIO, _('audio')),
+        (FILE_VIDEO, _('video')),
+        (FILE_PDF, _('pdf'))
+    )
+    product = models.ForeignKey('product', verbose_name=_('product'), related_name= 'files', on_delete=models.CASCADE)
     title = models.CharField(verbose_name=_('title'), max_length=50)
+    file_type = models.PositiveSmallIntegerField(_('file_type'), choices=FILE_TYPES, default=FILE_VIDEO)
     file = models.FileField(verbose_name=_('file'), upload_to='files/%y/%m/%d/')
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
@@ -44,3 +59,6 @@ class File(models.Model):
         db_table = 'file'
         verbose_name = _('file')
         verbose_name_plural = _('files')
+
+    def __str__(self):
+        return self.title
